0c16847d6aba3024e323f0929354c039073b92c4
maurice <maurice@maurices-MacBook-Pro.local>
2017-08-04 19:17:47 -0300

    Initial Commit

---
+++ NewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewTest
+//
+//  Created by maurice on 2017-08-04.
+//  Copyright © 2017 maurice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
